cffd7a51655eb927ea80c8ac774fd1235252e0f6
João Leme <110986672+joaoleme95@users.noreply.github.com>
2023-10-15 20:31:35 -0300

    ProjetoAndroid

--- app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnConfirmar: Button
     lateinit var listaRepositories: RecyclerView
     lateinit var githubApi: GitHubService
+    lateinit var repositoryAdapter: RepositoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,52 +28,85 @@ class MainActivity : AppCompatActivity() {
         getAllReposByUserName()
     }
 
-    // Metodo responsavel por realizar o setup da view e recuperar os Ids do layout
+    // Método responsável por realizar o setup da view e recuperar os IDs do layout
     fun setupView() {
-        //@TODO 1 - Recuperar os Id's da tela para a Activity com o findViewById
+        nomeUsuario = findViewById(R.id.editTextNomeUsuario)
+        btnConfirmar = findViewById(R.id.buttonConfirmar)
+        listaRepositories = findViewById(R.id.recyclerViewRepositories)
+        setupListeners()
     }
 
-    //metodo responsavel por configurar os listeners click da tela
+    // Método responsável por configurar os listeners de clique da tela
     private fun setupListeners() {
-        //@TODO 2 - colocar a acao de click do botao confirmar
+        btnConfirmar.setOnClickListener {
+            saveUserLocal()
+        }
     }
 
-
-    // salvar o usuario preenchido no EditText utilizando uma SharedPreferences
+    // Salvar o usuário preenchido no EditText usando SharedPreferences
     private fun saveUserLocal() {
-        //@TODO 3 - Persistir o usuario preenchido na editText com a SharedPref no listener do botao salvar
+        val usuario = nomeUsuario.text.toString()
+        val sharedPref = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString("usuario", usuario)
+        editor.apply()
     }
 
     private fun showUserName() {
-        //@TODO 4- depois de persistir o usuario exibir sempre as informacoes no EditText  se a sharedpref possuir algum valor, exibir no proprio editText o valor salvo
+        val sharedPref = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        val usuario = sharedPref.getString("usuario", null)
+        if (usuario != null) {
+            nomeUsuario.setText(usuario)
+        }
     }
 
-    //Metodo responsavel por fazer a configuracao base do Retrofit
+    // Método responsável por fazer a configuração base do Retrofit
     fun setupRetrofit() {
-        /*
-           @TODO 5 -  realizar a Configuracao base do retrofit
-           Documentacao oficial do retrofit - https://square.github.io/retrofit/
-           URL_BASE da API do  GitHub= https://api.github.com/
-           lembre-se de utilizar o GsonConverterFactory mostrado no curso
-        */
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        githubApi = retrofit.create(GitHubService::class.java)
     }
 
-    //Metodo responsavel por buscar todos os repositorios do usuario fornecido
+    // Método responsável por buscar todos os repositórios do usuário fornecido
     fun getAllReposByUserName() {
-        // TODO 6 - realizar a implementacao do callback do retrofit e chamar o metodo setupAdapter se retornar os dados com sucesso
+        btnConfirmar.setOnClickListener {
+            val usuario = nomeUsuario.text.toString()
+            val call = githubApi.getAllRepositoriesByUser(usuario)
+            call.enqueue(object : Callback<List<Repository>> {
+                override fun onResponse(call: Call<List<Repository>>, response: Response<List<Repository>>) {
+                    if (response.isSuccessful) {
+                        val repositories = response.body() ?: emptyList()
+                        setupAdapter(repositories)
+                    } else {
+                        // Tratar erro da chamada à API
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
+                    // Tratar falha na chamada à API
+                }
+            })
+        }
     }
 
-    // Metodo responsavel por realizar a configuracao do adapter
+    // Método responsável por configurar o adapter
     fun setupAdapter(list: List<Repository>) {
-        /*
-            @TODO 7 - Implementar a configuracao do Adapter , construir o adapter e instancia-lo
-            passando a listagem dos repositorios
-         */
+        repositoryAdapter = RepositoryAdapter(list)
+        repositoryAdapter.carItemLister = {
+            // Implemente a ação desejada ao clicar no item da lista
+        }
+        repositoryAdapter.btnShareLister = {
+            // Implemente a ação desejada ao clicar no botão Share
+        }
+
+        listaRepositories.layoutManager = LinearLayoutManager(this)
+        listaRepositories.adapter = repositoryAdapter
     }
 
-
-    // Metodo responsavel por compartilhar o link do repositorio selecionado
-    // @Todo 11 - Colocar esse metodo no click do share item do adapter
+    // Compartilhar o link do repositório selecionado
     fun shareRepositoryLink(urlRepository: String) {
         val sendIntent: Intent = Intent().apply {
             action = Intent.ACTION_SEND
@@ -84,9 +118,7 @@ class MainActivity : AppCompatActivity() {
         startActivity(shareIntent)
     }
 
-    // Metodo responsavel por abrir o browser com o link informado do repositorio
-
-    // @Todo 12 - Colocar esse metodo no click item do adapter
+    // Abrir o browser com o link informado do repositório
     fun openBrowser(urlRepository: String) {
         startActivity(
             Intent(
@@ -94,7 +126,5 @@ class MainActivity : AppCompatActivity() {
                 Uri.parse(urlRepository)
             )
         )
-
     }
-
 }
